1da86a94ed34946215f4764cefee6d7b7dce542c
Akoom Joshua <sandiego2643@gmail.com>
2023-11-16 11:38:15 +0100

    added a new font montserrat

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
-/** @type {import('tailwindcss').Config} */
+
+  /** @type {import('tailwindcss').Config} */
+
+  const { fontFamily } = require ('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: [],
+  content: [
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mont: ['var(--font-mont)'],
+        mono: ['var(--font-roboto-mono)'],
+      },
+    },
+  },
+  plugins: [],
+
   theme: {
     extend: {},
-  },
+  },  
   plugins: [],
 }
 
